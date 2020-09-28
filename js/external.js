d8f84b5f78e6389d5b4b6e566692953e34e2a23d
Caleb Wardlaw <calebwardlaw@gmail.com>
2020-09-28 11:19:27 -0500

    completed external and internal. not sanitized

--- js/external.js
+++ js/external.js
@@ -12,7 +12,7 @@ alert('Great, ' + userInput + ' is my favorite color too!');
 
 alert("How many days did you rent the following movies?");
 
-var theLittleMermaid = prompt('The Little Mermaid');
+var theLittleMermaid = (prompt('The Little Mermaid'));
 
 var brotherBear = prompt ('Brother Bear');
 
@@ -22,10 +22,10 @@ var rentalPrice = prompt("How much was rental per day?");
 
 var total = (theLittleMermaid * rentalPrice) + (brotherBear * rentalPrice) + ( hercules * rentalPrice);
 
-alert('Your spent ' + total + ' dollars on movie rentals.');
+alert('You spent ' + total + ' dollars on movie rentals.');
 
-// END OF EXC 1
-// BEGIN EXC 2
+// END OF PROBLEM 1
+// BEGIN PROBLEM 2
 
 var google = prompt('We heard you get paid well! What is your hourly rate at Google?');
 var googleHours = prompt ( 'Very nice! How many hours did you work this week?' );
@@ -34,28 +34,51 @@ var amazon = prompt('What is your pay at Amazon?');
 var amazonHours = prompt( ' Same hours as Google? If not, how many?');
 
 var facebook = prompt('... and how much does Facebook pay?')
-var facebookHours = prompt ('How many hours at Facebook this week?')
+var facebookHours = prompt ('How many hours were worked at Facebook this week?')
 
 var weeklyPay = (google * googleHours) + (amazon * amazonHours) + (facebook * facebookHours)
 
 alert ('You made ' + weeklyPay + ' this week!' ) //How do we get double input variables in JS? If I want to prompt once get hourly wages x,y, and z.
 
-//END OF EXC 2
-//BEGN EXC 3
+// END OF PROBLEM 2
+// BEGIN PROBLEM 3
 
-var seats = prompt('How many seats are left in the class?');
+alert ("Lets register you for class. Click okay if the statement is true or cancel if false. ")
 
-if (seats>=1) {
-    var fit = prompt ('Lucky you. The class has space! Does it fit your schedule? 1 for yes and 2 for no');
+var space = confirm('Is there space in the class?');
 
-        if (fit=1) {
-            alert('Excellent. You are registered!')
-        } else {
-            alert ('Scheduling conflict. Registration unavailable');
-        }
+if (space===false) {
 
-} else {
-    alert ( 'Class is full. Try again next semester');
+    alert("Unable to register.")
 }
 
-//Good to enroll
+if (space===true) {
+
+    var schedule = confirm('Is the time slot free?');
+
+        if (schedule===true) {
+
+            alert("You are registered.");
+    }
+}
+
+// END OF PROBLEM 3
+// BEGIN PROBLEM 4
+
+alert('You may be eligible for a discount!');
+
+var itemsPurchased = prompt("How many items did you purchase?");
+
+if(itemsPurchased>=2) {
+
+    var expireDate = confirm ("Does your offer expire after today? Okay for yes, cancel for no.")
+
+        if (expireDate===false) {
+
+            alert("Congratulations! We will apply the discount!");
+
+        } else { alert (" A discount cannot be applied at this time.") }
+
+} else { alert(" A discount cannot be applied at this time."); }
+
+//END OF PROBLEM 4
--- js/warmup.js
+++ js/warmup.js
@@ -1,11 +1,11 @@
 "use strict";
 
-console.log("Hello, Zek here")
+console.log("Hello, Zek here");
 
-console.log("Hello, Josh here")
+console.log("Hello, Josh here");
 
-console.log("Hello, Jeff here")
+console.log("Hello, Jeff here");
 
-console.log("Hello, Rayshard here")
+console.log("Hello, Rayshard here");
 
-console.log("Hello, Dylan here")
+console.log("Hello, Dylan here");
