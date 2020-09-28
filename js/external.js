56d40288d30086512483f7287ac9db37a73e9de3
Caleb Wardlaw <calebwardlaw@gmail.com>
2020-09-28 12:31:47 -0500

    completed js w/ html excercises

--- js/external.js
+++ js/external.js
@@ -6,19 +6,21 @@ alert("Welcome to my Website!");
 
 var userInput = prompt('What is your favorite color?');
 
-console.log('The user enetered: ' + userInput);
+console.log('The user entered: ' + userInput);
 
 alert('Great, ' + userInput + ' is my favorite color too!');
 
+//BEGIN PROBLEM 1
+
 alert("How many days did you rent the following movies?");
 
-var theLittleMermaid = (prompt('The Little Mermaid'));
+var theLittleMermaid = Number(prompt('The Little Mermaid'));
 
-var brotherBear = prompt ('Brother Bear');
+var brotherBear = Number(prompt('Brother Bear'));
 
-var hercules = prompt ('Hercules');
+var hercules = Number(prompt('Hercules'));
 
-var rentalPrice = prompt("How much was rental per day?");
+var rentalPrice = Number(prompt("How much was rental per day?"));
 
 var total = (theLittleMermaid * rentalPrice) + (brotherBear * rentalPrice) + ( hercules * rentalPrice);
 
@@ -27,29 +29,29 @@ alert('You spent ' + total + ' dollars on movie rentals.');
 // END OF PROBLEM 1
 // BEGIN PROBLEM 2
 
-var google = prompt('We heard you get paid well! What is your hourly rate at Google?');
-var googleHours = prompt ( 'Very nice! How many hours did you work this week?' );
+var google = Number(prompt('We heard you get paid well! What is your hourly rate at Google?'));
+var googleHours = Number(prompt ( 'Very nice! How many hours did you work this week?' ));
 
-var amazon = prompt('What is your pay at Amazon?');
-var amazonHours = prompt( ' Same hours as Google? If not, how many?');
+var amazon = Number(prompt('What is your pay at Amazon?'));
+var amazonHours = Number(prompt( ' Same hours as Google? If not, how many?'));
 
-var facebook = prompt('... and how much does Facebook pay?')
-var facebookHours = prompt ('How many hours were worked at Facebook this week?')
+var facebook = Number(prompt('... and how much does Facebook pay?'));
+var facebookHours = Number(prompt ('How many hours were worked at Facebook this week?'));
 
-var weeklyPay = (google * googleHours) + (amazon * amazonHours) + (facebook * facebookHours)
+var weeklyPay = (google * googleHours) + (amazon * amazonHours) + (facebook * facebookHours);
 
-alert ('You made ' + weeklyPay + ' this week!' ) //How do we get double input variables in JS? If I want to prompt once get hourly wages x,y, and z.
+alert ('You made ' + weeklyPay + ' this week!' ); //How do we get double input variables in JS? If I want to prompt once get hourly wages x,y, and z.
 
 // END OF PROBLEM 2
 // BEGIN PROBLEM 3
 
-alert ("Lets register you for class. Click okay if the statement is true or cancel if false. ")
+alert ("Lets register you for class. Click okay if the statement is true or cancel if false. ");
 
 var space = confirm('Is there space in the class?');
 
 if (space===false) {
 
-    alert("Unable to register.")
+    alert("Unable to register.");
 }
 
 if (space===true) {
@@ -65,15 +67,16 @@ if (space===true) {
 // END OF PROBLEM 3
 // BEGIN PROBLEM 4
 
+
 alert('You may be eligible for a discount!');
 
-var itemsPurchased = prompt("How many items did you purchase?");
+var itemsPurchased = Number(prompt("How many items did you purchase?"));
 
 if(itemsPurchased>=2) {
 
-    var expireDate = confirm ("Does your offer expire after today? Okay for yes, cancel for no.")
+    var expireDate = confirm ("Is your offer current? Okay for yes, cancel for no.")
 
-        if (expireDate===false) {
+        if (expireDate===true) {
 
             alert("Congratulations! We will apply the discount!");
 
