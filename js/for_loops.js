eaf7d8366de66df8c16c333ab93c71c1d349a564
Caleb Wardlaw <calebwardlaw@gmail.com>
2020-10-02 15:13:16 -0500

    completed multiplication table

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,12 +1,20 @@
-(function (){
-"use strict";
+// (function (){
+// "use strict";
 
 
 
 /*Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10*/
+    var i = 0
 
+    function showMultiplicationTable(number) {
+        while (i <= 10) {
+            console.log(i * number)
+            i++;
 
+        }
 
+    }
+    console.log(showMultiplicationTable(5))
 
 
 /*Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even. For example:*/
@@ -43,4 +51,4 @@
 
 
 
-})();
+// })();
