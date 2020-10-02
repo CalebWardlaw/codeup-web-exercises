79320b2e403b11388d575c5160c5d5c7827330cd
Caleb Wardlaw <calebwardlaw@gmail.com>
2020-10-02 14:42:06 -0500

    added instructions

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,4 +1,46 @@
 (function (){
 "use strict";
 
+
+
+/*Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10*/
+
+
+
+
+
+/*Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even. For example:*/
+
+
+
+
+
+/*Create a for loop that uses console.log to create the output shown below. NUMBER PYRAMID*/
+
+
+
+
+
+
+/*Create a for loop that uses console.log to create the output shown below. Countdown from 100 by 5 to 5*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })();
