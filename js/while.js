c7c1c04bea16fdc8f22c5e12c72af8b66b90682c
Caleb Wardlaw <calebwardlaw@gmail.com>
2020-10-04 17:53:42 -0500

    started ice cream problem

--- js/while.js
+++ js/while.js
@@ -1,12 +1,56 @@
+(function () {
+"use strict";
 
-var i = 1
-while(i<16){
-    for( var i = 1; i <= 16; i++){
-        console.log(Math.pow(2,i))
+/* While loop using console.log*/
+
+
+// var i = 1
+// while(i<16){
+//     for( var i = 1; i <= 16; i++){
+//         console.log(Math.pow(2,i))
+//     }
+// }
+
+//Why does this run despite the duplicate declaration notification? Can it be refactored to avoid it?
+
+
+
+
+var totalCones = Math.floor(Math.random() * 50) + 50;
+
+alert("Cone total is " + totalCones)
+
+do {
+    var sold = Math.floor(Math.random() * 5) + 1;
+    var remaining = (totalCones - sold)
+
+    function nextRound (sold){
+        return (totalCones - sold);
     }
-}
+
+    console.log(sold + " cones sold ...")
+    console.log(remaining + " cones remain")
 
 
+
+    // need to establish how many cones are remaining
+
+} while(remaining > 0)
+
+    //function takes in something and outputs something else
+
+
+
+
+
+
+
+
+
+
+
+
+})();
 
 
 
