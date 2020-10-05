4188c46e1a827a5c57d3cd0eaaca7e85f0b33cae
Caleb Wardlaw <calebwardlaw@gmail.com>
2020-10-05 08:30:08 -0500

    annotated questions in while, break_and_cont., and for_loops

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -5,30 +5,30 @@
 Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered. */
 
 
-    // var pickNumber = parseInt(prompt("Pick an odd number between 1 and 50"));
-    // while (true) {
-    //     if (isNaN(pickNumber)) {
-    //         alert("That's not a number, please retry.");
-    //     } else if (pickNumber < 1 || pickNumber > 50 || pickNumber % 2 === 0) {
-    //         alert("Invalid number");
-    //     } else {
-    //         break;
-    //     }
-    //
-    //     var pickNumber = parseInt(prompt("Pick an odd number between 1 and 50"));
-    //     }
-    //     console.log("Number to skip is: " + pickNumber)
-    //     for (var i = 0; i < 50; i++){
-    //     if(i === pickNumber){
-    //         console.log("Yikes! Skipping number: " + pickNumber)
-    //         continue;
-    //     }
-    //     if(i % 2 === 0){
-    //         continue;
-    //     }
-    //     if(i % 2 !== 0){
-    //         console.log("Here is an odd number: " + i)
-    //     }
-    // }
+    var pickNumber = parseInt(prompt("Pick an odd number between 1 and 50"));
+    while (true) {
+        if (isNaN(pickNumber)) {
+            alert("That's not a number, please retry.");
+        } else if (pickNumber < 1 || pickNumber > 50 || pickNumber % 2 === 0) {
+            alert("Invalid number");
+        } else {
+            break;
+        }
+
+    var pickNumber = parseInt(prompt("Pick an odd number between 1 and 50"));
+    }
+        console.log("Number to skip is: " + pickNumber)
+        for (var i = 0; i < 50; i++){
+        if(i === pickNumber){
+            console.log("Yikes! Skipping number: " + pickNumber)
+            continue;
+        }
+        if(i % 2 === 0){
+            continue;
+        }
+        if(i % 2 !== 0){
+            console.log("Here is an odd number: " + i)
+        }
+    }
 
 })();
--- js/while.js
+++ js/while.js
@@ -1,14 +1,15 @@
-
+(function (){
+    "use strict";
 
 /* While loop using console.log*/
 
 
-// var i = 1
-// while(i<16){
-//     for( var i = 1; i <= 16; i++){
-//         console.log(Math.pow(2,i))
-//     }
-// }
+var i = 1
+while(i<16){
+    for( var i = 1; i <= 16; i++){
+        console.log(Math.pow(2,i))
+    }
+}
 
 //Why does this run despite the duplicate declaration notification? Can it be refactored to avoid it?
 
@@ -44,21 +45,21 @@
 //
 //     //function takes in something and outputs something else
 
-var totalCones = Math.floor(Math.random() * 50) + 50;
-alert("Cone total is " + totalCones)
+// var totalCones = Math.floor(Math.random() * 50) + 50;
+// alert("Cone total is " + totalCones)
+//
+//
+//
+//
+//     var sold = Math.floor(Math.random() * 5) + 1;
+//     console.log(sold + " cones sold.");
+//
+//     while(){
+//
+//     }
 
 
-
-
-    var sold = Math.floor(Math.random() * 5) + 1;
-    console.log(sold + " cones sold.");
-
-    while(){
-
-    }
-
-
-
+})();
 
 
 
