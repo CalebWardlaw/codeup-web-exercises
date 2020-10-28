efc18ed4d6360125c182828da4c48a2b710cf8d6
Caleb Wardlaw <calebwardlaw@gmail.com>
2020-10-28 09:33:31 -0500

    uncommented while.js

--- js/while.js
+++ js/while.js
@@ -13,41 +13,41 @@ while(i<16){
 
 //Why does this run despite the duplicate declaration notification? Can it be refactored to avoid it?
 
-// var totalCones = Math.floor(Math.random() * 50) + 50;
-// var totalTransactions = 0
-//
-//     console.log("We need to sell " + totalCones + " cones.")
-//
-//     do{
-//
-//         var transaction = Math.floor(Math.random() * 5) + 1;
-//             //reassign value of
-//         // console.log(transaction + totalTransactions)
-//
-//         console.log(transaction + " cones sold.")
-//
-//         totalTransactions = transaction + totalTransactions
-//
-//         console.log(totalCones - totalTransactions + " cones remain.")
-//         if(totalCones - totalTransactions === 0 || transaction === (totalCones - totalTransactions)){
-//             console.log("Last " + (totalCones - totalTransactions) + " sold.")
-//             console.log ("Yay! I sold them all!")
-//             break;
-//         }
-//         if (totalCones - totalTransactions <= 0){
-//             console.log ("Yay! I sold them all!")
-//             break;
-//         } else
-//
-//         if (transaction >= (totalCones - totalTransactions)){
-//             console.log("I can't sell you " + transaction + " cones." + " I have " + (totalCones - totalTransactions) + " left.")
-//             console.log("But I will sell you " + (totalCones - totalTransactions))
-//             console.log("Last " + (totalCones - totalTransactions) + " sold."  )
-//             console.log("Yay! I sold them all!")
-//             break;
-//         }
-//     } while(totalTransactions < totalCones)
-//
+var totalCones = Math.floor(Math.random() * 50) + 50;
+var totalTransactions = 0
+
+    console.log("We need to sell " + totalCones + " cones.")
+
+    do{
+
+        var transaction = Math.floor(Math.random() * 5) + 1;
+            //reassign value of
+        // console.log(transaction + totalTransactions)
+
+        console.log(transaction + " cones sold.")
+
+        totalTransactions = transaction + totalTransactions
+
+        console.log(totalCones - totalTransactions + " cones remain.")
+        if(totalCones - totalTransactions === 0 || transaction === (totalCones - totalTransactions)){
+            console.log("Last " + (totalCones - totalTransactions) + " sold.")
+            console.log ("Yay! I sold them all!")
+            break;
+        }
+        if (totalCones - totalTransactions <= 0){
+            console.log ("Yay! I sold them all!")
+            break;
+        } else
+
+        if (transaction >= (totalCones - totalTransactions)){
+            console.log("I can't sell you " + transaction + " cones." + " I have " + (totalCones - totalTransactions) + " left.")
+            console.log("But I will sell you " + (totalCones - totalTransactions))
+            console.log("Last " + (totalCones - totalTransactions) + " sold."  )
+            console.log("Yay! I sold them all!")
+            break;
+        }
+    } while(totalTransactions < totalCones)
+
 
 
 
