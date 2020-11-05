fdd2e1d1c513f21bd32ba0c75c38cf8e473e64ed
Caleb Wardlaw <calebwardlaw@gmail.com>
2020-11-05 14:59:05 -0600

    switching projects

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -38,3 +38,20 @@ fogoDeChao.setPopup(popup)
 var restaurant = [tacosElGordo, miRancho, fogoDeChao]
 
 restaurant.forEach()
+
+function geocode(search, token) {
+    var baseUrl = 'https://api.mapbox.com';
+    var endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+        .then(function(res) {
+            return res.json();
+            // to get all the data from the request, comment out the following three lines...
+        }).then(function(data) {
+            return data.features[0].center;
+        });
+}
+
+geocode("506 E. Locust St., San Antonio, TX 78212", mapboxTokenTwo).then(function(result)){
+    console.log(result);
+    map.setCenter(result);
+}
